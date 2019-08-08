bb578062dc446c05480976e016202b076f898120
Daniel <ardekanicapital@gmail.com>
2019-08-07 22:48:16 -0400

    initial commit with added routes and view files

---
+++ src/views/Books.vue
@@ -0,0 +1,32 @@
+
+<template>
+  <div class="home">
+    <h1>Harry Potter Books</h1>
+    <div v-for="books in book">
+      <h2>{{ books.name }}</h2>
+      <img v-bind:src="books.url">
+      <p>Name: {{ books.name }}</p>
+      <p>Page: {{ books.page_numbers }}</p>.
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      books: []
+    };
+  },
+  created: function() {
+    axios.get("/api/photos").then(response => {
+      this.photos = response.data;
+    });
+  },
+  methods: {
+
+  }
+};
+</script>
